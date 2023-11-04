9ff9187fd462b699a4145a28ed7b6822c1b3505b
yuuki-kurose <pw1006@icloud.com>
2023-11-04 22:39:18 +0900

    Add: After the login authentication is successful, the user is redirected to the calendar page.
    ログイン認証が通ったら、カレンダーページへ遷移させる

---
+++ src/clnd/resources/js/Pages/Calender.jsx
@@ -0,0 +1,12 @@
+import React  from 'react';
+import Common from '../Layout/common';
+
+function calenderUserPage() {
+  return(
+    <Common>
+      カレンダーページです。
+    </Common>
+  )
+}
+
+export default calenderUserPage;
