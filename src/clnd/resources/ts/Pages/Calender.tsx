b2551bb3073a562ea67f7c0a72aca3699ba880ae
yuuki-kurose <pw1006@icloud.com>
2023-11-25 23:13:43 +0900

    Change: Moved to typescript.
    typescriptに移行

--- src/clnd/resources/ts/Pages/Calender.tsx
+++ src/clnd/resources/ts/Pages/Calender.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Calender from '../scss/calender.module.scss';
-import ScheduleForm from '../Pages/Schedule';
+import ScheduleForm from './Schedule';
 
 const CalenderUserPage = React.memo(function() {
 
--- src/clnd/vite.config.js
+++ src/clnd/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from "@vitejs/plugin-react"; 
+import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +9,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        react()
+        react(),
+        reactRefresh(),
     ],
 });
